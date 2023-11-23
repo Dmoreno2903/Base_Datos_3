d0a7afcf6656a8096fb38d2687de0a0bc141a152
SamuelMezaT <smezat@unal.edu.co>
2023-11-23 16:46:44 -0500

    Index

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trabajo final BD1</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+<div class="container mt-5">
+
+    <h1>Trabajo final BD1 - Compraventa de vehículos DIANSA</h1>
+
+    <div class="informacion">
+        <ul>
+            <li><b>Materia:</b> Bases de Datos I</li>
+            <li><b>Profesor:</b> Francisco Javier Moreno Arboleda</li>
+            <li><b>Institución:</b> Universidad Nacional de Colombia sede Medellín</li>
+            <li><b>Semestre:</b> 2023-2</li>
+        </ul>
+    </div>
+
+    <div class="integrantes">
+        <h2>Integrantes</h2>
+        <ul>
+            <li>Samuel Meza Tabares</li>
+            <li>Angel David Brand Londoño</li>
+            <li>Juan Diego Aguirre Moreno</li>
+        </ul>
+    </div>
+
+    <div class="modelo">
+        <h2>Modelo E-R </h2>
+        <p></p>
+        <img src="entidadrelacion.png">
+    </div>
+
+    <div class="enlaces">
+        <h2>Enlaces</h2>
+        <ul>
+            <li><a href="cliente/cliente.php">Entidad análoga a CASILLERO (TRANSACCIÓN)</a></li>
+            <li><a href="empresa/empresa.php">Entidad análoga a CLIENTE (CLIENTE)</a></li>
+            <li><a href="consultas/consulta1.php">Consulta 1</a></li>
+            <li><a href="consultas/consulta2.php">Consulta 2</a></li>
+            <li><a href="busqueda/busqueda1.php">Búsqueda 1</a></li>
+            <li><a href="busqueda/busqueda2.php">Búsqueda 2</a></li>
+        </ul>
+    </div>
+</div>
+</body>
+
+</html>
